2cfe4f89a10797b4c022d0d989ea17a4a38faa4d
whowouldwin <leranetwork@gmail.com>
2025-03-24 00:36:04 +0300

    feat: implement getTime, getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,11 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
@@ -46,8 +49,26 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const day = new Date(date).getDay();
+  switch (day) {
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+      return 'Friday';
+    case 6:
+      return 'Saturday';
+    case 0:
+      return 'Sunday';
+    default:
+      return '';
+  }
 }
 
 /**
@@ -99,7 +120,7 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * Returns true if a given date is within a specified range, including both the start and end dates.
  *
  * @typedef {{
- * start: string, // The start date in ISO 8601 format (e.g., 'YYYY-MM-DD').
+ * start: string, // The start date in ISO 8601 format (e.g., "YYYY-MM-DD").
  * end: string    // The end date in ISO 8601 format.
  * }} DatePeriod
  *
@@ -199,8 +220,8 @@ function getQuarter(/* date */) {
  * The start and end dates of the period are inclusive.
  *
  * @typedef {{
- * start: string, // The start date in 'DD-MM-YYYY' format.
- * end: string    // The end date in 'DD-MM-YYYY' format.
+ * start: string, // The start date in "DD-MM-YYYY" format.
+ * end: string    // The end date in "DD-MM-YYYY" format.
  * }} DatePeriod
  *
  * @param {DatePeriod} period - The start and end dates of the period.
