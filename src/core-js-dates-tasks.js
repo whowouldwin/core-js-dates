b589438291a089525213889ef11846ca43691a35
whowouldwin <leranetwork@gmail.com>
2025-03-24 21:41:44 +0300

    feat: implement getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -239,8 +239,33 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  const day = date.getDate();
+  if (day >= 13) {
+    month += 1;
+    if (month > 11) {
+      month = 0;
+      year += 1;
+    }
+  }
+  let found = false;
+  let result;
+  while (!found) {
+    const checkedDate = new Date(year, month, 13);
+    if (checkedDate.getDay() === 5) {
+      result = checkedDate;
+      found = true;
+    } else {
+      month += 1;
+      if (month > 11) {
+        month = 0;
+        year += 1;
+      }
+    }
+  }
+  return result;
 }
 
 /**
